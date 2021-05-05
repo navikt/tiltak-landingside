80a0cbbbbf3b8e4921799e3f39d6b7a4e2405ffa
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-05-05 12:00:17 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      HELLO WORLD
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
