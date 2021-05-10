593de6e7dde50b111d7d36df221557ec12e32fa3
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-05-11 00:17:04 +0200

    oppdatert root ved aa legge til '/' for scroll-links

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import SommerJobb from "./SommerJobb";
-import ContextProvider from "./ContextProvider";
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import SommerJobb from './SommerJobb';
+import ContextProvider from './ContextProvider';
+import UrlAppend from './UrlAppend';
 
 // main
 function App() {
-  return (
-    <BrowserRouter>
-      <ContextProvider>
-        <div>
-          <Switch>
-            <Route path="/tiltak/sommerjobb" exact={true}>
-              <SommerJobb />
-            </Route>
-          </Switch>
-        </div>
-      </ContextProvider>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <ContextProvider>
+                <div>
+                    <Switch>
+                        <Route path="/tiltak/sommerjobb/" exact={true}>
+                            <UrlAppend>
+                                <SommerJobb />
+                            </UrlAppend>
+                        </Route>
+                    </Switch>
+                </div>
+            </ContextProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
