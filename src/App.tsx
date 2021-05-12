8dc3492a2e6c878be7df508fcce33c3ada37f558
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-05-12 15:16:49 +0200

    lagt til amplitude oppsett

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import SommerJobb from './SommerJobb';
 import ContextProvider from './ContextProvider';
 import UrlAppend from './UrlAppend';
+import { registrerBesokside } from './utils/amplitude-utils';
 
 // main
 function App() {
+    useEffect(() => {
+        registrerBesokside();
+    });
     return (
         <BrowserRouter>
             <ContextProvider>
