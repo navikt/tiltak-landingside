8a07a073b8992699aeed304ca002f61eacca97eb
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-05-12 16:14:17 +0200

    oppdatert keys til amp

--- src/utils/amplitude-utils.ts
+++ src/utils/amplitude-utils.ts
@@ -1,7 +1,7 @@
 import amplitude from './amplitudeInstance';
 import { LogReturn } from 'amplitude-js';
 
-const miljo = process.env.NODE_ENV ? 'p' : 'dev';
+const miljo = process.env.NODE_ENV ? 'p-' : 'dev-';
 const defaultkey = '#tiltak-landingside-'.concat(miljo);
 
 export const registrerBesokside = (): LogReturn => amplitude.logEvent(defaultkey.concat('besok'));
